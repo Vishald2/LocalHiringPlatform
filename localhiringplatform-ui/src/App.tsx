7957e39af7534afb049f20195f14ac981c64e576
Vishal <vishald24@yahoo.com>
2026-05-30 17:31:51 +0530

    Working

--- localhiringplatform-ui/src/App.tsx
+++ localhiringplatform-ui/src/App.tsx
@@ -1,8 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+
 function App() {
     return (
-        <div>
-            <h1>Local Hiring Platform</h1>
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegisterPage />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
