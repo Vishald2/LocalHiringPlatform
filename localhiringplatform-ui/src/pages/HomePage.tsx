8a2af003c1f08f077a0ae482408fb13ff2e2c0e6
Vishal <vishald24@yahoo.com>
2026-05-30 18:52:17 +0530

    working fine

--- localhiringplatform-ui/src/pages/HomePage.tsx
+++ localhiringplatform-ui/src/pages/HomePage.tsx
@@ -1,3 +1,9 @@
 export default function HomePage() {
-    return <h1>Home Page</h1>;
+    return ( 
+        <>
+    <h1>This Is Home Page</h1>
+            <span>abc</span>
+<div>This is div</div>
+        </>
+    );
 }
