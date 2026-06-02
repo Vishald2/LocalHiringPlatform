53a09b84d9c39b21f156f329381701f071248827
Vishal <vishald24@yahoo.com>
2026-06-02 17:49:01 +0530

    test commit

--- Program.cs
+++ Program.cs
@@ -7,3 +7,8 @@ var app = builder.Build();
 app.MapGet("/", () => Results.Text("LocalHiringPlatform API running"));
 
 app.Run();
+/*
+ * This is the entry point of the LocalHiringPlatform API application. It sets up a simple web server using ASP.NET Core.
+ * The application responds to GET requests at the root URL ("/") with a plain text message indicating that the API is running.
+ * The application is configured to run on .NET 10.0, as specified in the project file.
+ */
