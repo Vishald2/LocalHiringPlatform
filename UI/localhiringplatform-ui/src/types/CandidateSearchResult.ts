d585f91d3c9cc7c22a3053ec2fb539cca86b1ce2
Vishal <vishald24@yahoo.com>
2026-06-14 16:37:40 +0530

    Done. Working Fine. Candidate Search.

---
+++ UI/localhiringplatform-ui/src/types/CandidateSearchResult.ts
@@ -0,0 +1,19 @@
+﻿export interface CandidateSearchResult {
+    candidateProfileId: string;
+
+    fullName: string;
+
+    email: string;
+
+    mobileNumber: string;
+
+    city: string;
+
+    totalExperienceYears: number;
+
+    resumeFileName?: string;
+
+    resumeFilePath?: string;
+
+    isOpenToWork: boolean;
+}
