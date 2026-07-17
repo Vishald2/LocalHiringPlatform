0e468143e0916cf085bc9359ec2d3ec73a800c68
Vishal <vishald24@yahoo.com>
2026-07-17 07:54:08 +0530

    Added Azure Service Bus configuration model

---
+++ LocalHiringPlatform.ServiceBus/Configuration/ServiceBusOptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalHiringPlatform.ServiceBus.Configuration
+{
+    public class ServiceBusOptions
+    {
+        public const string SectionName = "AzureServiceBus";
+
+        public string ConnectionString { get; set; } = string.Empty;
+
+        public string QueueName { get; set; } = string.Empty;
+    }
+}
