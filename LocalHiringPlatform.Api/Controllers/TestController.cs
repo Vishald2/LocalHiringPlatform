c06b36b4b75c2cb09ba06683e9c68ce5ea034a6a
Vishal <vishald24@yahoo.com>
2026-07-22 16:37:14 +0530

    Test Controller

--- LocalHiringPlatform.Api/Controllers/TestController.cs
+++ LocalHiringPlatform.Api/Controllers/TestController.cs
@@ -12,28 +12,19 @@ namespace LocalHiringPlatform.Api.Controllers;
 [Route("api/[controller]")]
 public class TestController : ControllerBase
 {
-
-   // private readonly IRedisCacheService _redisCacheService;
     private readonly ILogger _logger;
     private IPromptService _promptService;
     private readonly IServiceBusPublisher _serviceBusPublisher;
 
     public TestController(
-      //  IRedisCacheService redisCacheService,
         ILogger<TestController> logger,
         IPromptService promptService,
         IServiceBusPublisher serviceBusPublisher
         )
     {
-      //  _redisCacheService = redisCacheService;
         _logger = logger;
         _promptService = promptService;
-        _serviceBusPublisher = serviceBusPublisher;
-
-
-
-
-        
+        _serviceBusPublisher = serviceBusPublisher;  
     }
 
     [HttpPost("publish")]
@@ -50,71 +41,4 @@ public class TestController : ControllerBase
 
         return Ok();
     }
-
-
-        [HttpGet("prompt")]
-    public async Task<IActionResult> TestPrompt()
-    {
-        var prompt =
-            await _promptService.GetPromptAsync(
-                "JobSearchIntentPrompt.txt");
-
-        return Ok(prompt);
-    }
-
-    [HttpGet]
-    public IActionResult Test()
-    {
-        throw new Exception("This is a test exception for logging purposes.");
-        return Ok(new
-        {
-            Message = "You are authenticated"
-        });
-    }
-
-    //[HttpGet("redis-test")]
-    //public async Task<IActionResult> RedisTest()
-    //{
-    //    await _redisCacheService.SetAsync(
-    //        "TestKey",
-    //        "Hello Redis",
-    //        TimeSpan.FromMinutes(5));
-
-    //    var value =
-    //        await _redisCacheService.GetAsync<string>(
-    //            "TestKey");
-
-    //    return Ok(value);
-    //}
-
-    //[HttpGet("redis-ping")]
-    //public async Task<IActionResult> RedisPing()
-    
-    //{
-    //    try
-    //    {
-    //        await _redisCacheService.SetAsync(
-    //            "Ping",
-    //            "Pong");
-
-    //        var value =
-    //            await _redisCacheService.GetAsync<string>(
-    //                "Ping");
-
-    //        return Ok(new
-    //        {
-    //            Success = true,
-    //            Value = value
-    //        });
-    //    }
-    //    catch (Exception ex)
-    //    {
-    //        return Ok(new
-    //        {
-    //            Success = false,
-    //            Message = ex.Message,
-    //            InnerException = ex.InnerException?.Message
-    //        });
-    //    }
-    //}
 }
